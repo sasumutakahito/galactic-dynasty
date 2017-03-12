3881a6d5818506f47c12590471a24b375dff9f68
Andrew Pamment <apamment@yandex.com>
2017-03-12 17:24:07 +1000

    Modified reset.sh

--- reset.sh
+++ reset.sh
@@ -2,3 +2,12 @@
 cp users.db3.orig users.db3
 cp msgs.db3.orig msgs.db3
 cp interbbs.db3.orig interbbs.db3
+if [ -e ibbs_scores.ans ]
+then
+	rm ibbs_scores.ans
+fi
+
+if [ -e scores.ans ]
+then
+        rm scores.ans
+fi
