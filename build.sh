1f06a749b9c7805845353fc7710cb5553fafaa89
Andrew Pamment <apamment@yandex.com>
2017-03-25 18:55:37 +1000

    Attempt to fix structure packing on windows

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ make
 
 cd ..
 
-gcc -c main.c -o main.o -I./odoors/
-gcc -c interbbs2.c -o interbbs2.o 
-gcc -c inih/ini.c -o inih/ini.o
+gcc -c main.c -o main.o -I./odoors/ -mno-ms-bitfields
+gcc -c interbbs2.c -o interbbs2.o  -mno-ms-bitfields
+gcc -c inih/ini.c -o inih/ini.o -mno-ms-bitfields
 gcc -o GalacticDynasty main.o interbbs2.o inih/ini.o odoors/libs-`uname -s`/libODoors.a -lsqlite3
