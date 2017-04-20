6a4c19cda377bc95a8d2fb104f9de569d552ea25
Andrew Pamment <apamment@yandex.com>
2017-04-20 20:49:03 +1000

    Fix for linux

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ make
 
 cd ..
 
-gcc -c main.c -o main.o -I./odoors/ -I/usr/local/include/lua5.3
+gcc -c main.c -o main.o -I./odoors/ -I/usr/include/lua5.3
 gcc -c interbbs2.c -o interbbs2.o 
 gcc -c inih/ini.c -o inih/ini.o 
 gcc -o GalacticDynasty main.o interbbs2.o inih/ini.o odoors/libs-`uname -s`/libODoors.a -lsqlite3 -llua5.3
