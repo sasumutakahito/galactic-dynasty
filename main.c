c90fd55b85fe5ed6da6b362a5eaca5d9788157d6
Andrew Pamment <apamment@yandex.com>
2017-09-16 12:56:19 +1000

    another attempt to fix on windows grrr

--- main.c
+++ main.c
@@ -2478,15 +2478,17 @@ void game_loop(player_t *player)
 						while (!bank_done) {
 							if (player->bank_balance >= 0) {
 #ifdef MSC_VER
-								od_printf("Your current bank balance is `bright green`%I64d `white`credits. Interest is 0.1%% (%d credits) per day .\r\n", player->bank_balance, (int)((float)gPlayer->bank_balance * 0.001f));
+								od_printf("Your current bank balance is `bright green`%I64d `white`credits.\r\n", player->bank_balance);
+								od_printf("Interest is 0.1%% (%d credits) per day .\r\n", (int)((float)gPlayer->bank_balance * 0.001f));
 #else								
-								od_printf("Your current bank balance is `bright green`%lld `white`credits. Interest is 0.1%% (%d credits) per day.\r\n", player->bank_balance, (int)((float)gPlayer->bank_balance * 0.001f));
+								od_printf("Your current bank balance is `bright green`%lld `white`credits.\r\nInterest is 0.1%% (%d credits) per day.\r\n", player->bank_balance, (int)((float)gPlayer->bank_balance * 0.001f));
 #endif
 							} else {
 #ifdef MSC_VER
-								od_printf("Your current bank balance is `bright red`%I64d `white`credits. Interest is 5%% (%d credits) per day.\r\n", player->bank_balance, abs((int)((float)gPlayer->bank_balance * 0.05f)));
+								od_printf("Your current bank balance is `bright red`%I64d `white`credits.\r\n", player->bank_balance);
+								od_printf("Interest is 5%% (%d credits) per day.\r\n", abs((int)((float)gPlayer->bank_balance * 0.05f)));
 #else
-								od_printf("Your current bank balance is `bright red`%lld `white`credits. Interest is 5%% (%d credits) per day.\r\n", player->bank_balance, abs((int)((float)gPlayer->bank_balance * 0.05f)));
+								od_printf("Your current bank balance is `bright red`%lld `white`credits.\r\nInterest is 5%% (%d credits) per day.\r\n", player->bank_balance, abs((int)((float)gPlayer->bank_balance * 0.05f)));
 #endif
 							}
 							od_printf("Your current allowed overdraft is %u credits.\r\n", (calculate_score(player) * 100) / 2);
