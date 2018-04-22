56acf9fc8b51dbc847df25744d7d3bbcd4e51162
Andrew Pamment <apamment@yandex.com>
2018-04-22 19:13:11 +1000

    Work on pirate attacks

--- events.lua
+++ events.lua
@@ -74,45 +74,37 @@ if (gd_in_protection() == 0) then
 
         local pirate_troops;
         if troops / 100 >= 1 then
-            pirate_troops = math.floor(troops + (math.random(troops / 100) - (troops / 100 / 2)));
+            pirate_troops = math.floor(troops + (math.random(math.floor(troops / 100)) - (troops / 100 / 2)));
         else 
             pirate_troops = 0;
         end
         local pirate_fighters;
         if fighters / 100 >= 1 then 
-            piirate_fighters = math.floor(fighters + (math.random(fighters / 100) - (fighters / 100 / 2)));
+            piirate_fighters = math.floor(fighters + (math.random(math.floor(fighters / 100)) - (fighters / 100 / 2)));
         else
             pirate_fighters = 0;
         end
 
         local pirate_generals;
         if generals / 100 >= 1 then
-            pirate_generals = math.floor(generals + (math.random(generals / 100) - (generals / 100 / 2)));
+            pirate_generals = math.floor(generals + (math.random(math.floor(generals / 100)) - (generals / 100 / 2)));
         else
             pirate_generals = 0;
         end
-        -- do battle
 
-        local attack = (3 * pirate_troops + 1 * pirate_fighters) +  ((3 * pirate_troops + 1 * pirate_fighters) * (pirate_generals / 100));
-        local defence = 10 * troops;
-
-        attack = attack + ( 1 * pirate_troops + 4 * pirate_fighters +  ((1 * pirate_troops + 4 * pirate_fighters) * (pirate_generals / 100)));
-        defence = defence + (25 * fighters);
-
-        local victory_chance = (attack / (attack + defence)) ;
         local battle = math.random(100);
 
-        if battle < (victory_chance * 100) then
+        if battle < 50 then
             -- pirate victory
             local food = gd_get_food()
             local population = gd_get_population()
             local credits = gd_get_credits()
             local planets = gd_get_planets();
 
-            local difference = math.random(5);
-            local dtroops = math.floor(troops - (troops * (difference / 100)));
-            local dgenerals = math.floor(generals - (generals * (difference / 100)));
-            local dfighters = math.floor(fighters - (fighters * (difference / 100)));
+            local difference = math.random(50);
+            local dtroops = math.floor(pirate_troops - (pirate_troops * (difference / 100)));
+            local dgenerals = math.floor(pirate_generals - (pirate_generals * (difference / 100)));
+            local dfighters = math.floor(pirate_fighters - (pirate_fighters * (difference / 100)));
             
             if dtroops > troops then
                 dtroops = troops
@@ -133,20 +125,20 @@ if (gd_in_protection() == 0) then
             local plunder_planets = math.floor(planets /  10);
 
             gd_set_food(food - plunder_food);
-            gd_set_population(poplation - plunder_population);
+            gd_set_population(population - plunder_population);
             gd_set_credits(credits - plunder_credits);
             plunder_planets = gd_destroy_planets(plunder_planets);
 
             gd_print_red("PIRATE ATTACK!!")
             gd_print_red(dtroops .. " troops, " .. dgenerals .. " generals, " .. dfighters .. " defence stations destroyed.");
             gd_print_red(plunder_food .. " food, " .. plunder_population .. " citizens, " .. plunder_credits .. " credits stolen.");
-            gd_print_red(plunder_planets .. " planets destroyed.");
+            gd_print_red(math.floor(plunder_planets) .. " planets destroyed.");
 
         else
-            local difference = math.random(5);
-            local dtroops = math.floor(troops - (troops * (difference / 100)));
-            local dgenerals = math.floor(generals - (generals * (difference / 100)));
-            local dfighters = math.floor(fighters - (fighters * (difference / 100)));
+            local difference = math.random(50);
+            local dtroops = math.floor(pirate_troops - (pirate_troops * (difference / 100)));
+            local dgenerals = math.floor(pirate_generals - (pirate_generals * (difference / 100)));
+            local dfighters = math.floor(pirate_fighters - (pirate_fighters * (difference / 100)));
             
             if dtroops > troops then
                 dtroops = troops
@@ -160,8 +152,10 @@ if (gd_in_protection() == 0) then
             gd_set_troops(troops - dtroops);
             gd_set_generals(generals - dgenerals);
             gd_set_defence_stations(fighters - dfighters);
-            gd_print_red("PIRATE ATTACK!!")
-            gd_print_red(dtroops .. " troops, " .. dgenerals .. " generals, " .. dfighters .. " defence stations destroyed.");        
+            if dtroops > 0 or dgenerals > 0 or dfighters > 0 then
+                gd_print_red("PIRATE ATTACK!!")
+                gd_print_red(dtroops .. " troops, " .. dgenerals .. " generals, " .. dfighters .. " defence stations destroyed.");
+            end
         end
     end
 end
